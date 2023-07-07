2473abd87f752db0221b7291c0736ca61e7d6cbd
Vriddhi Sharma <vriddhisharma3011@gmail.com>
2023-07-07 22:24:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const hour= document.getElementById("hours");
+const min=document.getElementById("minutes");
+const sec=document.getElementById("seconds");
+const ap=document.getElementById("ampm")
+
+function liveClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+    
+    if(h>12){
+        ampm="PM"
+    }
+
+    h = h<10 ? "0"+ h : h;
+    m = m<10 ? "0"+ m : m;
+    s = s<10 ? "0"+ s : s;
+
+    hour.innerText=h;
+    min.innerText=m;
+    sec.innerText=s;
+    ap.innerText=ampm;
+
+    setTimeout(() => {
+        liveClock();
+    },1000);
+}
+
+liveClock();
